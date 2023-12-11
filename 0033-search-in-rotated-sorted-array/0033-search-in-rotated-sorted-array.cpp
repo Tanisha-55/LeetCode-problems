ea14bbf48067a331d7fc30157d073243f0e6942d
Tanisha Kumari <107767459+Tanisha-55@users.noreply.github.com>
2023-12-11 20:03:45 +0530

    Time: 3 ms (65.48%), Space: 11.2 MB (97.66%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left=0,right=nums.size()-1;
+        while(left<=right){
+            if(nums[left]==target){
+                return left;
+            }
+            else if(nums[right]==target){
+                return right;
+            }
+
+            else if(target<nums[right]){
+                right--;
+            }
+            else if(target>nums[left]){
+                left++;
+            }
+            else{
+                break;
+            }
+        }
+        return -1;
+    }
+};
